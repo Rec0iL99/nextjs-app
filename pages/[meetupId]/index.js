7d4530b8ddb0d37184cb4f0e9c22cfba62f00fc4
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-05-27 15:16:54 +0530

    Component and styles

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://www.fairobserver.com/wp-content/uploads/2020/12/Oman-2.jpg"
+      title="First meetup"
+      address="Some address 5, Some City, Some Country"
+      description="This is the first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
