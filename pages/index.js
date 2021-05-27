d1011f24755460b099352bb6d67ba96429278be4
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-05-27 16:32:03 +0530

    Pre rendering and getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -17,8 +18,35 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const HomePage = (props) => {
+  /* 
+    Two render cycles 
+    1. Component imp function runs with loadedMeetups as empty array
+    2. then 2nd cycle useEffect funct runs and loadedMeetups has array now
+    But nextJS just takes the first render cycle results and puts it in html (SEO problem here since loadedMeetups is empty)
+  */
+  // const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  // useEffect(() => {
+  //   setLoadedMeetups(DUMMY_MEETUPS);
+  // }, []);
+  // No need of above due to getStatic props
+
+  return <MeetupList meetups={props.meetups} />;
+};
+
+// Way to move data fetching away from client side and in server side or build process
+export const getStaticProps = async () => {
+  // This funct will not run or be shown to the client
+  // It is run when npm run build is done
+  // Fetch from api
+  // Connect to DB
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 };
 
 export default HomePage;
